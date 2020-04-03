7a219dd919fd2a7abfaf66040ce4a985225c3169
Raja Sharan Mamidala <rajasharan@gmail.com>
2020-04-03 17:12:53 -0400

    ptop using htop

--- .oh-my-zsh/custom/my_aliases.zsh
+++ .oh-my-zsh/custom/my_aliases.zsh
@@ -4,3 +4,10 @@ alias cpu="ps axc -o cmd,%mem,%cpu --sort=-%cpu"
 alias mem="ps axc -o cmd,%mem,%cpu --sort=-%mem"
 alias watch="watch -n 0.5 -d "
 alias gls="git ls-files --full-name"
+alias sudo="sudo -E env \"PATH=$PATH\""
+alias ports="sudo lsof -PiTCP -sTCP:LISTEN"
+alias dua="du -a"
+
+ptop() {
+    htop --pid=$(pgrep -i "$@" | paste -sd, -)
+}
